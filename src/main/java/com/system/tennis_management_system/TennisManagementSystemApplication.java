23c7204417b32441f9cfa84a09fe48e541d12cdb
Asmit11 <sandeepgahatraj9@gmail.com>
2023-08-09 19:33:57 +0545

    first push

---
+++ src/main/java/com/system/tennis_management_system/TennisManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.system.tennis_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/tennis_management_system/TennisManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.tennis_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
