17b91b088189a3e7fe8784cc62d71dfd663f05b2
Asmit11 <sandeepgahatraj9@gmail.com>
2023-08-10 00:39:02 +0545

    added contact repo

---
+++ src/main/java/com/system/tennis_management_system/Repo/ContactRepo.java
@@ -0,0 +1,10 @@
+package com.system.tennis_management_system.Repo;
+
+import com.system.tennis_management_system.entity.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepo extends JpaRepository<Contact ,Integer> {
+
+}
+
+
